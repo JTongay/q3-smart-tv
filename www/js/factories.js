56639cee7897ccaed0954c16f9c03c8553e461cb
Joseph Tongay <dirk.tongay@gmail.com>
2016-11-17 11:05:13 -0600

    added port to ip

--- www/js/factories.js
+++ www/js/factories.js
@@ -55,7 +55,7 @@
 // // };
 app.factory('socket',function(socketFactory){
 	//Create socket and connect to http://chat.socket.io
- 	var myIoSocket = io.connect();
+ 	var myIoSocket = io.connect('http://10.8.67.47:8080');
 
   var mySocket = socketFactory({
     	ioSocket: myIoSocket
