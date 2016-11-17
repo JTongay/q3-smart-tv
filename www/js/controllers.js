3720090073bdce0e9bee7bb0c75eb180ecd1ae9b
Joseph Tongay <dirk.tongay@gmail.com>
2016-11-16 20:22:26 -0600

    this should work....

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -80,10 +80,10 @@ angular.module('starter.controllers', [])
 // console.log(socket);
 //   console.log(io());
 // $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
-  $scope.socket = socket.on('connect', function (data){
-    socket.emit('remote');
-
-  })
+  // $scope.socket = socket.on('connect', function (data){
+  //   socket.emit('remote');
+  //
+  // })
 
   $scope.search.getVideos = function(query){
     $http.get("https://www.googleapis.com/youtube/v3/search?order=viewcount&part=snippet&q=" + query + "&type=video+&videoDefinition=high&key=" + "AIzaSyDOB7yYD2E_NK1P0HnPrgCN_hKmP-DYSIo" + "&maxResults=25").then(function(results){
@@ -91,6 +91,13 @@ angular.module('starter.controllers', [])
       var results = results.data.items;
       $scope.search.results = results;
       // console.log(socket.emit('video'));
+    })
+  }
+
+  $scope.search.watch = function(id){
+    socket.emit('video', {
+      action: 'play',
+      video_id: id
     })
   }
 }])
